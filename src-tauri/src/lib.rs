e6634f683a0b6f46e87ee0ebd5f20a6534bfd5a3
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2025-01-25 14:15:31 +0530

    (/usr/share/games/fortunes/tao) % A good walker leaves no tracks; A good speaker makes no slips; A good reckoner needs no tally. A good door needs no lock, Yet no one can open it. Good binding requires no knots, Yet no one can loosen it.
    Therefore the sage takes care of all men
    And abandons no one.
    He takes care of all things
    And abandons nothing.
    
    This is called "following the light."
    
    What is a good man?
    A teacher of a bad man.
    What is a bad man?
    A good man's charge.
    If the teacher is not respected,
    And the student not cared for,
    Confusion will arise, however clever one is.
    This is the crux of mystery.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,36 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri_plugin_clipboard_manager::ClipboardExt;
+use tauri_plugin_barcode_scanner;
+use tauri_plugin_biometric;
+use tauri_plugin_opener;
+
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::cbwrite]
+fn cbwrite(text: &str) -> String {
+j   app.clipboard()
+        .write_text("Tauri is awesome!".to_string())
+        .unwrap();
+}
+
+#[tauri::cbread]
+fn cbread() -> String {
+    // Read content from clipboard
+    let content = app.clipboard().read_text();
+    println!("{:?}", content.unwrap());
+    content
+}
+// Prints "Tauri is awesome!" to the terminal
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_biometric::init())
+        .plugin(tauri_plugin_barcode_scanner::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
